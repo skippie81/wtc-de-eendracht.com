75acbbc31f23548a9a57ef5348bc360332ed5a49
skippie81 <31068429+skippie81@users.noreply.github.com>
2026-09-20 13:58:02 +0200

    first 2027 update

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function includeHTML() {
+    var elements = document.getElementsByTagName("*");
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var file = element.getAttribute("w3-include-html");
+        if (file) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        element.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        element.innerHTML = "Page not found.";
+                    }
+                    element.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhr.open("GET", file, true);
+            xhr.send();
+            return;
+        }
+    }
+}
+includeHTML();
